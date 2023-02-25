f2e94ef573305ac33ad2d10cd7bbcef457c6e5a4
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2023-02-24 23:28:23 -0600

    create quick frontend ui for plants list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [plants, setPlants] = useState([])
+
+useEffect(() => {
+    async function getPlants() {
+      try {
+        const res = await axios.get('http://localhost:3001/plants');
+        const plants = res.data.plants;
+        console.log(res.data.plants);
+        setPlants(plants);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getPlants();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {plants.map(plant=>{
+          return (
+            <div key={plant.id} className='Plant'>
+            <img className="Plant-img" alt={plant.name} src={plant.imgUrl}/>
+            <h3>{plant.name}</h3>
+            <p>{plant.description}</p>
+          </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
