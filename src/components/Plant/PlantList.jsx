b6c564dada1a6d68432b0ea8df1956d29f393af8
Matt Baltzell <matthew.j.baltzell@gmail.com>
2023-03-02 15:39:33 -0600

    Add navigation links to each Plant-list thumbnail

--- src/components/Plant/PlantList.jsx
+++ src/components/Plant/PlantList.jsx
@@ -1,11 +1,21 @@
+import React from "react";
 import "./Plant.css";
+import { useNavigate } from "react-router-dom";
 
 const PlantList = ({ plants }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="Plant-list">
       {plants.map((plant) => {
         return (
-          <div key={plant.id} className="Plant-list__thumbnail">
+          <div
+            key={plant.id}
+            onClick={() => {
+              navigate(`/plants/${plant.id}`);
+            }}
+            className="Plant-list__thumbnail"
+          >
             <img
               className="Plant-list__img"
               alt={plant.name}
