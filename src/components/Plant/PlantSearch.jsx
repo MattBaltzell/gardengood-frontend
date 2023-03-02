9af099d7aff52ca27338fecb161716981f41f82f
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2023-03-01 20:52:11 -0600

    fix search feature

--- src/components/Plant/PlantSearch.jsx
+++ src/components/Plant/PlantSearch.jsx
@@ -1,34 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Plant.css";
+import "../Form/Form.css";
 import PlantList from "./PlantList";
 
 const PlantSearch = ({ plants, search }) => {
   const INITIAL_STATE = { searchTerm: "" };
   const [formData, setFormData] = useState(INITIAL_STATE);
 
-  const handleChange = (e) => {
+  const handleChange = async (e) => {
     const { name, value } = e.target;
     setFormData((data) => ({ ...data, [name]: value }));
+    search(value.trim() || undefined);
   };
+
+  useEffect(() => {
+    return () => {
+      setFormData(INITIAL_STATE);
+    };
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    search(formData.searchTerm);
-    setFormData(INITIAL_STATE);
+    search(formData.searchTerm.trim() || undefined);
   };
 
   return (
     <main className="Plant-search">
       <h1>Find a Plant</h1>
-      <form className="Plant-search_form" onSubmit={handleSubmit}>
+      <form className="Form Plant-search__form" onSubmit={handleSubmit}>
         <input
+          className="Form__input--text"
           name="searchTerm"
           onChange={handleChange}
           type="text"
           value={formData.searchTerm}
-          placeholder="Enter plant name"
+          placeholder="Search plant name"
         />
-        <button>Search</button>
       </form>
       <PlantList plants={plants} />
     </main>
