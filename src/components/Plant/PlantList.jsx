39f30b549fd2cf92c09d1aa2e92a3814274ca628
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2023-03-01 20:52:36 -0600

    style form and plant list

--- src/components/Plant/PlantList.jsx
+++ src/components/Plant/PlantList.jsx
@@ -5,13 +5,13 @@ const PlantList = ({ plants }) => {
     <div className="Plant-list">
       {plants.map((plant) => {
         return (
-          <div key={plant.id} className="Plant-list_thumbnail">
+          <div key={plant.id} className="Plant-list__thumbnail">
             <img
-              className="Plant-list_img"
+              className="Plant-list__img"
               alt={plant.name}
               src={plant.imgUrl}
             />
-            <h3 className="Plant-list_name">{plant.name}</h3>
+            <h3 className="Plant-list__name">{plant.name}</h3>
           </div>
         );
       })}
