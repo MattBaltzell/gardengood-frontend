7a43b0aedcbc962812d23a322aa494cf501ad3f4
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2023-03-27 18:35:20 -0500

    remove unneccessary loading from private routes

--- src/components/routes-nav/PrivateRoutes.jsx
+++ src/components/routes-nav/PrivateRoutes.jsx
@@ -1,29 +1,10 @@
 import React, { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import UserContext from "../auth/UserContext";
-import { BallTriangle } from "react-loader-spinner";
 
 const PrivateRoutes = () => {
   const currUser = useContext(UserContext);
-  return !currUser ? (
-    <>
-      <BallTriangle
-        height={100}
-        width={100}
-        radius={5}
-        color="var(--color-primary-400)"
-        ariaLabel="ball-triangle-loading"
-        wrapperClass={{}}
-        wrapperStyle=""
-        visible={true}
-      />
-      <p className="pulsing">Loading...</p>
-    </>
-  ) : currUser ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/login" />
-  );
+  return currUser ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoutes;
